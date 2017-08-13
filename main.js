4995569538e429c9d7ed8eb72adfb61b2043d3a5
Cristian Herrera <cristianherrera@Cristians-MacBook-Pro.local>
2017-08-12 18:29:00 -0700

    JavaScript is working

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-var results = document.querySelector(".results-container")
+var results = document.querySelector(".results-container") //grabbing HTML Elements to use in here
 
-var searchBox = document.querySelector("#searchBar").addEventListener('keydown', function(event){
+var searchBox = document.querySelector("#searchBar")
 
-  if(event.keyCode === 13){
-    return(displayData(event.target.value));
+searchBox.addEventListener('keydown', function(event){ //adding Event listener for when a keyboard button is pushed
+
+  if(event.keyCode === 13){ //13 is the keycode for Return-Enter
+
+    return(getInfo(event.target.value)); //return is running the function below
+
   }
 
 })
@@ -11,20 +15,18 @@ var searchBox = document.querySelector("#searchBar").addEventListener('keydown',
 getInfo("")
 
 function getInfo(event){
-fetch(`https://api.soundcloud.com/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=${event}`)
+fetch(`https://api.soundcloud.com/tracks?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=${event}`) //API address used to grab info
       .then( function(response) {
-        return response.json()
-        console.log(json)
+        return response.json() //returns API in Json format
       }).then(function(data){
-
               moreInfo(data)
 
       })
 }
 
 
-function moreInfo(data){
+function moreInfo(data){ //looping through values in Objects
   for(x=0; x<data.length; x++){
-
+    console.log(data[x].title)
   }
 }
